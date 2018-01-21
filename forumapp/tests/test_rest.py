4819027edc00f6bed594453991f132e5d5227e10
kamildydo <dydokamil@gmail.com>
2018-01-21 14:20:44 +0100

    Created two test for forum_get and thread_post

--- forumapp/tests/test_rest.py
+++ forumapp/tests/test_rest.py
@@ -1,31 +1,53 @@
 import json
 from unittest import TestCase
 
+from django.contrib.auth.models import User
 from rest_framework.test import APIRequestFactory, force_authenticate, APIClient
 
-from forumapp.models import Forum
+from forumapp.models import Forum, Thread, ForumSection
 
 
 class ForumSectionModelTest(TestCase):
     @classmethod
-    def setUpTestData(cls):
-        pass
+    def setUpClass(cls):
+        section = ForumSection.objects.create(name='section1')
+
+        Forum.objects.create(
+            name='Forum1',
+            description='Desc1',
+            section=section
+        )
+        Forum.objects.create(
+            name='Forum2',
+            description='Desc2',
+            section=section
+        )
 
     def test_forum_get(self):
-        Forum.objects.create(name='Forum1', description='Desc1')
-        Forum.objects.create(name='Forum2', description='Desc2')
-
         client = APIClient()
-        response = client.get('/forumapp/rest/forums')
+        response = client.get('/forumapp/rest/forums/')
         self.assertEqual(json.loads(response.content), [
             {
                 'name': 'Forum1',
                 'description': 'Desc1',
-                'section': None
+                'section': 1
             },
             {
                 'name': 'Forum2',
                 'description': 'Desc2',
-                'section': None
+                'section': 1
             }
         ])
+
+    def test_create_thread(self):
+        client = APIClient()
+        response = client.post(
+            '/forumapp/rest/threads/', {
+                'name': 'thread1',
+                'forum': 1,
+                'pinned': True,
+            }
+        )
+
+        # user not logged-in
+        self.assertEquals(response.status_code, 403)
